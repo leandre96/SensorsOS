42672d8eec14308f6d3bd5801e131fb559269c83
leandre96 <leancast@espol.edu.ec>
2020-08-28 12:33:49 -0500

    update

--- monitoreo.c
+++ monitoreo.c
@@ -14,11 +14,52 @@
 #include <signal.h>
 #include <semaphore.h>
 
+#define MAXSTR 50 /* Cantidad de caracteres en la linea del archivo csv */
 
+FILE *archivoCSV;                    /* Puntero al archivo csv */
+const char s[2] = ",";               /* Separador ',' de las líneas en el archivo csv */
+int cantSensores = 0;
+char **enunciadoSensores;
 int main(void){
     key_t claveGlobal = ftok("/bin/man",35);
     int shmid = shmget(claveGlobal,sizeof(int),IPC_CREAT | 0660);
-
+    archivoCSV = fopen("sensores.csv", "r"); /* Leemos el archivo csv */
+    while (fgets(content, MAXSTR, archivoCSV) != NULL){           /* Mientras haya una línea por leer en el archivo csv */
+    content[strlen(content) - 1] = '\0'; /* Se elimina el salto de línea */
+    char *tkn;                           /* Creamos un token para ejecutar el split */
+    int id, tipoS, th, comm;             /* Se crean las variables que almacenaran los datos del sensor */
+    tkn = strtok(content, s);            /* Se crea un iterador de la línea del archivo */
+    int split = 1;                       /* Se crea una variable de referencia de la posición del cursor de tkn */
+    while (tkn != NULL)
+    { /* Se itera al token hasta que apunte a NULL */
+      switch (split)
+      {
+      case 1:
+        id = atoi(tkn); /* Se asigna el id del sensor */
+        break;
+      case 2:
+        tipoS = atoi(tkn); /* Se asigna el tipo del sensor */
+        break;
+      case 3:
+        th = atoi(tkn); /* Se asigna el th del sensor */
+        break;
+      case 4:
+        comm = atoi(tkn); /* Se asigna el puerto de comunicación del sensor */
+        break;
+      }
+      tkn = strtok(NULL, s); /* iteramos el token */
+      split++;               /* Se aumenta el valor del cursor */
+    }
+    char tipo[MAXSTR];
+    if (tipoS >= 5){
+        tipo = "cooperativo";
+    }
+    else {
+        tipo = "competitivo";
+    }
+    sprintf(enunciadoSensores[cantSensores],"Sensor %s %d :%d",tipo,id,th);
+    cantSensores++;
+  }
     int opcion;
 
     do
